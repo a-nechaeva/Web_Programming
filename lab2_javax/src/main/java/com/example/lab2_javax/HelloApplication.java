2880c063e40e32a397237f4021cd9400c8a1d2c8
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-11-21 03:56:23 +0300

    time to sleep

---
+++ lab2_javax/src/main/java/com/example/lab2_javax/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.lab2_javax;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
