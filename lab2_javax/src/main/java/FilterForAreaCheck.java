169acfa6e8d5d147582e0bb8c1d8b9c4cd0e0b96
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-12-03 14:52:32 +0300

    Update FilterForAreaCheck.java

--- lab2_javax/src/main/java/FilterForAreaCheck.java
+++ lab2_javax/src/main/java/FilterForAreaCheck.java
@@ -2,12 +2,10 @@ import javax.servlet.Filter;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.ArrayList;
 
 // фильтер для того, чтобы не допускать прямого обращения к AreaCheckServlet
 public class FilterForAreaCheck implements Filter {
     private FilterConfig filterConfig;
-    private static ArrayList<String> servlets; // ранилище сервлетов
     @Override
     public void init(FilterConfig fConfig) throws SecurityException{
         filterConfig = fConfig;
@@ -20,12 +18,7 @@ public class FilterForAreaCheck implements Filter {
             HttpServletRequest req = (HttpServletRequest) request;
             // Парсим адрес в строку
             String r = req.getRequestURL().toString();
-            //извлекаем наименование сервлеты
-            String serv = null;
             if (!r.contains("AreaCheckServlet")) {
-               /* ServletContext ctx = filterConfig.getServletContext();
-                RequestDispatcher dispatcher = ctx.getRequestDispatcher("");*/
-
                 chain.doFilter(request, response);
                 return;
             }
