bd5ca99834eb97259ca54542673fa130b48001ba
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-12-06 17:34:46 +0300

    work ^. .%

--- lab2_jakarta/src/main/webapp/renew_scr.js
+++ lab2_jakarta/src/main/webapp/renew_scr.js
@@ -135,29 +135,36 @@ function detectClick() {
 }
 
 function setVisiblePoint(){
-    let point = document.getElementById("point")
-    point.setAttribute('cx',300 + cordX)
-    point.setAttribute('cy',300 - cordY)
-    point.setAttribute("visibility","visible")
-
-
-    if (pointsX.length > 0) {
+       if (pointsX.length > 0) {
         //здесь должно быть добавление точек
+           let ns = "http://www.w3.org/2000/svg"
+           let svg = document.getElementById('picture');
         for (let i = 0; i < pointsX.length; i++) {
-
-            let ns = "http://www.w3.org/2000/svg"
-            let svg = document.getElementById('picture');
             let circle = document.createElementNS(ns, 'circle');
             circle.setAttribute('r', 5);
             circle.setAttribute('cx', pointsX[i]);
             circle.setAttribute('cy', pointsY[i]);
-            circle.setAttribute('fill', 'wheat');
+            circle.setAttribute('fill', 'midnightblue');
             circle.setAttribute('class', 'old');
             svg.append(circle);
             // see: https://ru.stackoverflow.com/questions/563297/svg-%D0%94%D0%B8%D0%BD%D0%B0%D0%BC%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%BE%D0%B3%D1%80%D1%83%D0%B7%D0%BA%D0%B0-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%83
 
         }
     }
+
+   /* let point_cur = document.createElementNS(ns, 'circle');
+    point_cur.setAttribute('r', 5);
+    point_cur.setAttribute('cx', 300 + cordX);
+    point_cur.setAttribute('cy', 300 - cordY);
+    point_cur.setAttribute('fill', 'darkmagenta');
+    point_cur.setAttribute('stroke', 'magenta')
+    point_cur.setAttribute('class', 'old');
+    svg.append(point_cur);*/
+
+    let point = document.getElementById("point")
+    point.setAttribute('cx',300 + cordX)
+    point.setAttribute('cy',300 - cordY)
+    point.setAttribute("visibility","visible")
 
     pointsX.push(300 + cordX)
     pointsY.push(300 - cordY)
