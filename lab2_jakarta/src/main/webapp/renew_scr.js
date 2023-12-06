4e1618ff0f37b5af37a021695ee82744ce00fc3f
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-12-06 17:16:17 +0300

    wow точки сохраняются ^. .%
    как же я люблю веб)

--- lab2_jakarta/src/main/webapp/renew_scr.js
+++ lab2_jakarta/src/main/webapp/renew_scr.js
@@ -152,6 +152,7 @@ function setVisiblePoint(){
             circle.setAttribute('cx', pointsX[i]);
             circle.setAttribute('cy', pointsY[i]);
             circle.setAttribute('fill', 'wheat');
+            circle.setAttribute('class', 'old');
             svg.append(circle);
             // see: https://ru.stackoverflow.com/questions/563297/svg-%D0%94%D0%B8%D0%BD%D0%B0%D0%BC%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%BE%D0%B3%D1%80%D1%83%D0%B7%D0%BA%D0%B0-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%83
 
@@ -168,7 +169,14 @@ function unsetVisiblePoint(){
     point.setAttribute('cy',300)
     point.setAttribute("visibility","hidden")
 
-    pointsX.
+    for (let i = 0; i < pointsX.length; i++) {
+        let old_points = document.getElementsByClassName('old');
+        for (let p of old_points) {
+            p.setAttribute("visibility", "hidden");
+        }
+    }
+    pointsX = []
+    pointsY = []
 }
 
 function convertCoordinates() {
