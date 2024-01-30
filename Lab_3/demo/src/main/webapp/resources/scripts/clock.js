21c0a3182e60f4f977af5af3d3ec8bedba74724f
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-01-30 15:52:36 +0300

    start page in process...

---
+++ Lab_3/demo/src/main/webapp/resources/scripts/clock.js
@@ -0,0 +1,21 @@
+/*function clock() {
+    let date = new Date(),
+        hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+        minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+        seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds()
+
+    document.getElementById('clock').innerText = hours + ":" +
+        minutes + ":" + seconds
+}
+setTimeout(clock, 0)
+setInterval(clock, 10000)
+
+ */
+var myVar = setInterval(function() {
+    myTimer();
+}, 1000);
+
+function myTimer() {
+    var d = new Date();
+    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+}
